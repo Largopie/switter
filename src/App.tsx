59ad7e3864d35bcaabed54d33dc955a081f54452
Largopie <106071687+Largopie@users.noreply.github.com>
2023-12-04 20:29:03 +0900

    💄 [UI] GlobayStyles 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const Wrapper = styled.div`
+  height: 100vh;
+`;
+
+// basic color #A08CDD
+const GlobalStyles = createGlobalStyle`
+${reset}
+* {
+  box-sizing: border-box;
+}
+body {
+  background: #fff;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`;
+
 function App() {
+  
   return (
-    <>Initial commit</>
-  )
+    <Wrapper>
+      <GlobalStyles />
+    </Wrapper>
+  );
 }
 
-export default App
+export default App;
